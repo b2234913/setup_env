91869d3a9ee44c85d621737d8c4b9093b9d7ebe5
Shooter <b2234913@gmail.com>
2022-03-15 16:25:52 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+# install zsh
+sudo apt install zsh -y
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+# install plug-in
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+sed -i "s/plugins=(git)/plugins=(git extract z zsh-syntax-highlighting zsh-autosuggestions)/g" ~/.zshrc
+source ~/.zshrc
